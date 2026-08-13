18e51881a1fb718b873839a5769071e8bd7235d2
kagemya <yukikage43tt@gmail.com>
2026-08-13 14:29:20 +0900

    feat: ボタンコンポーネントとルーティングの追加 #1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
 
+import { LinkButton } from "../components/LinkButton";
+ 
 export default function Home() {
   return (
-    <div>こんにちは</div>
+    <main>
+      <h1>トップページ</h1>
+        <LinkButton link="/about" color="#ff5722">
+          アバウトページへ進む
+        </LinkButton>
+    </main>
   );
 }
